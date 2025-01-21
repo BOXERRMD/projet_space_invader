7e297b092668d7ecc629cc45c01edf6516a48ace
BOXER <130167557+BOXERRMD@users.noreply.github.com>
2025-01-21 15:28:15 +0100

    [UPDATE]

--- information_jeu.py
+++ information_jeu.py
@@ -5,5 +5,5 @@ Les informations du jeu.
 window_longueur = 640
 window_largeur = 480
 
-vaisseau_posX = 50
-vaisseau_posY = 50
+vaisseau_posX = window_longueur/2
+vaisseau_posY = window_largeur - 30
